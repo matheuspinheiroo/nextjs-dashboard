11a5f854c66cd204fa89516d58b2ce9497b96a1d
Matheus Pinheiro <matheuspinheiro2503@gmail.com>
2026-01-11 18:29:54 -0300

    Server_3

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,6 @@
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
- 
+import { Lusitana } from 'next/font/google';
+
 export const lusitana = Lusitana({
-  weight: ['400', '700'],
   subsets: ['latin'],
+  weight: ['400', '700'],
 });
